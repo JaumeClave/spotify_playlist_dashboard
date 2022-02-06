a8b36fc58f73f4f9a4860194a4522c021e8cba93
JaumeCD <j.clavedomenech@gmail.com>
2022-02-06 18:48:09 +0000

    Streamlit Secrets Management

--- src/visualization/spotify_compare.py
+++ src/visualization/spotify_compare.py
@@ -390,8 +390,48 @@ spotify = SpotifyAPI(client_id, client_secret)
 access_token = spotify.access_token
 sp = spotipy.Spotify(auth = access_token)
 
+############ EHA
 
-st.write("success")
+Types_of_Features = ("acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "tempo", "valence")
+
+st.title("Spotify Features App")
+Name_of_Artist = st.text_input("Artist Name")
+Name_of_Feat = st.selectbox("Feature", Types_of_Features)
+button_clicked = st.button("OK")
+
+
+Data = spotify.search({"artist": f"{Name_of_Artist}"}, search_type="track")
+
+need = []
+for i, item in enumerate(Data['tracks']['items']):
+    track = item['album']
+    track_id = item['id']
+    song_name = item['name']
+    popularity = item['popularity']
+    need.append((i, track['artists'][0]['name'], track['name'], track_id, song_name, track['release_date'], popularity))
+
+Track_df = pd.DataFrame(need, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity'))
+
+access_token = spotify.access_token
+
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+endpoint = "https://api.spotify.com/v1/audio-features/"
+
+Feat_df = pd.DataFrame()
+for id in Track_df['Id'].iteritems():
+    track_id = id[1]
+    lookup_url = f"{endpoint}{track_id}"
+    #print(lookup_url)
+    ra = requests.get(lookup_url, headers=headers)
+    audio_feat = ra.json()
+    #print(audio_feat)
+    Features_df = pd.DataFrame(audio_feat, index=[0])
+    Feat_df = Feat_df.append(Features_df)
+    #print(Feat_df)
+
+st.table(Feat_df)
 
 # ROW 1 ------------------------------------------------------------------------
 
